8879543a4fecd3c4bbcaaee77ea268c5436f8053
Guido Cavallo <SrShark@users.noreply.github.com>
2018-05-08 20:52:09 -0300

    agregado strategy a clase Materia

--- src/main/java/com/plan111/modelo/Materia.java
+++ src/main/java/com/plan111/modelo/Materia.java
@@ -4,16 +4,14 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "Materia")
 @Getter @Setter @NoArgsConstructor
 public class Materia {
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer idMateria;
     @Column(name = "nombre")
     private String nombre;
